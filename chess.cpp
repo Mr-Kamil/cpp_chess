6afcd89db1e65abe6f2ecff16c7f024ca2f34d53
Mr_Kamil <kamil.bylinka9@gmail.com>
2024-10-22 15:54:14 +0200

    chess.cpp refactored slightly

--- chess.cpp
+++ chess.cpp
@@ -17,8 +17,6 @@ bits:    1  0  0  0  0  0  0  0  0  1     bytes: 0
             A  B  C  D  E  F  G  H
 )";
 
-
-
 const Bitboard FILE_A = 0x01'01'01'01'01'01'01'01ULL;
 const Bitboard FILE_B = 0x02'02'02'02'02'02'02'02ULL;
 const Bitboard FILE_C = 0x04'04'04'04'04'04'04'04ULL;
@@ -39,15 +37,15 @@ const Bitboard RANK_8 = 0xFF'00'00'00'00'00'00'00ULL;
 
 const Bitboard BOUNDARIES = FILE_A | FILE_H | RANK_1 | RANK_8; // 0xFF'81'81'81'81'81'81'FFULL;
 
-
 Bitboard white_board = 0ULL;
 Bitboard black_board = 0ULL;
 
-Bitboard enemy_board = 0x00'00'00'00'00'04'00'00ULL;
-Bitboard allies_board = 0ULL;
+Bitboard enemy_board = 0x00'00'00'00'00'08'00'00ULL;
+Bitboard allies_board = 0x00'00'00'00'00'00'40'00ULL;
 
 
-void print_bitboard_as_bytes(Bitboard board) {
+void print_bitboard_as_bytes(Bitboard board)
+{
     std::cout << "Bitboard in hex: 0x" 
               << std::hex << std::setw(16) << std::setfill('0') 
               << board << std::endl;
@@ -81,49 +79,35 @@ int check_move_occupied(Bitboard move)
 }
 
 
-Bitboard slide_rook(Bitboard rook_position, int shift, Bitboard boundary_mask) {
-    Bitboard rooks_moves = 0ULL;
-    Bitboard move = rook_position;
+Bitboard slide_piece(Bitboard piece_position, int shift, Bitboard boundary_mask, bool single_move=false) 
+{
+    Bitboard piece_moves = 0ULL;
+    Bitboard move = piece_position;
 
     while (!(move & boundary_mask)) {
         move = (shift > 0) ? (move << shift) : (move >> -shift);
-
         int move_occupation = check_move_occupied(move);
+
         if (move_occupation == 1) break; 
-        rooks_moves |= move;                   
-        if (move_occupation == 2) break; 
+        piece_moves |= move;                   
+        if (move_occupation == 2) break;
+        if (single_move) break;
     }
 
-    return rooks_moves;
+    return piece_moves;
 }
 
 
-Bitboard generate_all_rooks_moves(Bitboard rook_board) {
+Bitboard generate_all_rooks_moves(Bitboard rook_board) 
+{
     Bitboard rooks_moves = 0ULL;
 
-    rooks_moves |= slide_rook(rook_board, -1, FILE_A);
-    rooks_moves |= slide_rook(rook_board, 1, FILE_H);
-    rooks_moves |= slide_rook(rook_board, -8, RANK_1);
-    rooks_moves |= slide_rook(rook_board, 8, RANK_8);
+    rooks_moves |= slide_piece(rook_board, -1, FILE_A);
+    rooks_moves |= slide_piece(rook_board, 1, FILE_H);
+    rooks_moves |= slide_piece(rook_board, -8, RANK_1);
+    rooks_moves |= slide_piece(rook_board, 8, RANK_8);
 
     return rooks_moves;
-}
-
-
-Bitboard slide_bishop(Bitboard bishop_position, int shift, Bitboard boundary_mask) {
-    Bitboard bishops_moves = 0ULL;
-    Bitboard move = bishop_position;
-
-    while (!(move & boundary_mask)) {
-        move = (shift > 0) ? (move << shift) : (move >> -shift);
-
-        int move_occupation = check_move_occupied(move);
-        if (move_occupation == 1) break; 
-        bishops_moves |= move;                   
-        if (move_occupation == 2) break; 
-    }
-
-    return bishops_moves;
 }
 
 
@@ -131,29 +115,12 @@ Bitboard generate_all_bishops_moves(Bitboard bishop_board)
 {
     Bitboard bishop_moves = 0ULL;
 
-    bishop_moves |= slide_rook(bishop_board, -9, FILE_A | RANK_8);
-    bishop_moves |= slide_rook(bishop_board, 9, FILE_H | RANK_8);
-    bishop_moves |= slide_rook(bishop_board, -7, RANK_1 | FILE_H);
-    bishop_moves |= slide_rook(bishop_board, 7, RANK_8 | FILE_A);
+    bishop_moves |= slide_piece(bishop_board, 7, FILE_A | RANK_8);
+    bishop_moves |= slide_piece(bishop_board, 9, FILE_H | RANK_8);
+    bishop_moves |= slide_piece(bishop_board, -9, RANK_1 | FILE_H);
+    bishop_moves |= slide_piece(bishop_board, -7, RANK_1 | FILE_A);
 
     return bishop_moves;
-}
-
-
-Bitboard slide_knight(Bitboard knight_position, int shift, Bitboard boundary_mask) {
-    Bitboard knights_moves = 0ULL;
-    Bitboard move = knight_position;
-
-    if (!(move & boundary_mask)) {
-        move = (shift > 0) ? (move << shift) : (move >> -shift);
-
-        int move_occupation = check_move_occupied(move);
-        if (move_occupation == 1); 
-        knights_moves |= move;                   
-        if (move_occupation == 2); 
-    }
-
-    return knights_moves;
 }
 
 
@@ -161,20 +128,28 @@ Bitboard generate_all_knights_moves(Bitboard knights_board)
 {
     Bitboard knights_moves = 0ULL;
 
-    knights_moves |= slide_knight(knights_board, 15, RANK_8 | RANK_7 | FILE_A);
-    knights_moves |= slide_knight(knights_board, 17, RANK_8 | RANK_7 | FILE_H);
-    knights_moves |= slide_knight(knights_board, 6, RANK_8 | FILE_A | FILE_B);
-    knights_moves |= slide_knight(knights_board, 10, RANK_8 | FILE_G | FILE_H);
-    knights_moves |= slide_knight(knights_board, -15, RANK_1 | RANK_2 | FILE_H);
-    knights_moves |= slide_knight(knights_board, -17, RANK_1 | RANK_2 | FILE_H);
-    knights_moves |= slide_knight(knights_board, -6, FILE_H | FILE_G | RANK_1);
-    knights_moves |= slide_knight(knights_board, -10, FILE_A | FILE_B | RANK_1);
+    knights_moves |= slide_piece(knights_board, 15, RANK_8 | RANK_7 | FILE_A, true);
+    knights_moves |= slide_piece(knights_board, 17, RANK_8 | RANK_7 | FILE_H, true);
+    knights_moves |= slide_piece(knights_board, 6, RANK_8 | FILE_A | FILE_B, true);
+    knights_moves |= slide_piece(knights_board, 10, RANK_8 | FILE_G | FILE_H, true);
+    knights_moves |= slide_piece(knights_board, -15, RANK_1 | RANK_2 | FILE_H, true);
+    knights_moves |= slide_piece(knights_board, -17, RANK_1 | RANK_2 | FILE_H, true);
+    knights_moves |= slide_piece(knights_board, -6, FILE_H | FILE_G | RANK_1, true);
+    knights_moves |= slide_piece(knights_board, -10, FILE_A | FILE_B | RANK_1, true);
 
     return knights_moves;
 }
 
 
-Bitboard slide_pawn(Bitboard pawn_position, int shift, Bitboard boundary_mask) {
+Bitboard generate_all_queens_moves(Bitboard queen_board)
+{
+    Bitboard queens_moves = generate_all_bishops_moves(queen_board) | generate_all_rooks_moves(queen_board);
+    return queens_moves;
+}
+
+
+Bitboard slide_pawn(Bitboard pawn_position, int shift, Bitboard boundary_mask) 
+{
     Bitboard pawns_moves = 0ULL;
     Bitboard move = pawn_position;
 
@@ -190,7 +165,8 @@ Bitboard slide_pawn(Bitboard pawn_position, int shift, Bitboard boundary_mask) {
 }
 
 
-Bitboard slide_double_pawn(Bitboard pawn_position, int shift) {
+Bitboard slide_double_pawn(Bitboard pawn_position, int shift) 
+{
     Bitboard pawns_moves = 0ULL;
     Bitboard move_double = pawn_position;
     Bitboard square_passed = pawn_position;
@@ -209,7 +185,8 @@ Bitboard slide_double_pawn(Bitboard pawn_position, int shift) {
 }
 
 
-Bitboard capture_pawn(Bitboard pawn_position, int shift, Bitboard boundary_mask) {
+Bitboard capture_pawn(Bitboard pawn_position, int shift, Bitboard boundary_mask) 
+{
     Bitboard pawns_moves = 0ULL;
     Bitboard move = pawn_position;
 
@@ -245,26 +222,7 @@ Bitboard generate_all_pawns_moves(Bitboard pawn_board, bool white_to_move=true)
             pawns_moves |= slide_double_pawn(pawn_board, -16);
         }
     }
-
-
     return pawns_moves;
-}
-
-
-Bitboard slide_king(Bitboard king_position, int shift, Bitboard boundary_mask) {
-    Bitboard kings_moves = 0ULL;
-    Bitboard move = king_position;
-
-    if (!(move & boundary_mask)) {
-        move = (shift > 0) ? (move << shift) : (move >> -shift);
-
-        int move_occupation = check_move_occupied(move);
-        if (move_occupation == 1); 
-        kings_moves |= move;                   
-        if (move_occupation == 2); 
-    }
-
-    return kings_moves;
 }
 
 
@@ -272,19 +230,12 @@ Bitboard generate_all_kings_moves(Bitboard king_board)
 {
     Bitboard king_moves = 0ULL;
 
-    king_moves |= slide_king(king_board, -1, FILE_A);
-    king_moves |= slide_king(king_board, 1, FILE_H);
-    king_moves |= slide_king(king_board, -8, RANK_1);
-    king_moves |= slide_king(king_board, 8, RANK_8);
+    king_moves |= slide_piece(king_board, -1, FILE_A, true);
+    king_moves |= slide_piece(king_board, 1, FILE_H, true);
+    king_moves |= slide_piece(king_board, -8, RANK_1, true);
+    king_moves |= slide_piece(king_board, 8, RANK_8, true);
 
     return king_moves;
-}
-
-
-Bitboard generate_all_queens_moves(Bitboard queen_board)
-{
-    Bitboard queens_moves = generate_all_bishops_moves(queen_board) | generate_all_rooks_moves(queen_board);
-    return queens_moves;
 }
 
 
@@ -297,8 +248,17 @@ int main()
     print_bitboard_as_bytes(demonstrative_board);
     print_graphic_bitboard(demonstrative_board);
 
-    Bitboard test_board_1 = 0x0000000000000200ULL;
+    std::cout << "ALLIES BOARD: " << std::endl;
+    print_bitboard_as_bytes(allies_board);
+    print_graphic_bitboard(allies_board);
+
+    std::cout << "ENEMY BOARD: " << std::endl;
+    print_bitboard_as_bytes(enemy_board);
+    print_graphic_bitboard(enemy_board);
+
+    Bitboard test_board_1 = 0x0000000000000400ULL;
     std::cout << "TEST BOARD: " << std::endl;
+    print_bitboard_as_bytes(test_board_1);
     print_graphic_bitboard(test_board_1);
 
     std::cout << "ROOK: " << std::endl;
