3b28227ab7667be01d2b4d67e7000a48c11e19eb
Mr_Kamil <kamil.bylinka9@gmail.com>
2024-10-29 21:36:32 +0100

    uci loop draft implementation

--- chess.cpp
+++ chess.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include <sstream>
 
 typedef uint64_t Bitboard;
 
@@ -479,6 +481,116 @@ Bitboard generate_all_pawns_moves(Bitboard pawn_board, bool white_to_move=true)
     pawns_moves |= en_passant_move(pawn_board);
 
     return pawns_moves;
+}
+
+
+void reset_board()
+{
+
+}
+
+
+void start_new_game() 
+{
+
+}
+
+void apply_move()
+{
+
+}
+
+void get_best_move()
+{
+
+}
+
+void identify_engine() 
+{
+    std::cout << "id name TryChess 0.1\n";
+    std::cout << "id author Kamil Bylinka\n";
+}
+
+void handle_isready() 
+{
+    std::cout << "readyok\n";
+}
+
+void handle_uci() 
+{
+    identify_engine();
+    std::cout << "uciok\n";
+}
+
+void handle_ucinewgame() 
+{
+    start_new_game();
+    std::cout << "New game initialized\n";
+}
+
+void handle_position(std::stringstream &ss) 
+{
+    std::string subcommand;
+    ss >> subcommand;
+
+    if (subcommand == "startpos") {
+        reset_board();
+        std::string moves;
+        if (ss >> moves && moves == "moves") {
+            std::string move;
+            while (ss >> move) {
+                apply_move();
+            }
+        }
+    } else if (subcommand == "fen") {
+        std::string fen;
+        std::getline(ss, fen);
+        apply_move();
+    }
+}
+
+void handle_go(std::stringstream &ss) 
+{
+    std::string searchCommand;
+    ss >> searchCommand;
+    get_best_move();
+
+    std::cout << "bestmove xXxX" << std::endl;
+}
+
+void handle_quit()
+{
+    std::cout << "Exiting..." << std::endl;
+}
+
+void UCI_loop() {
+    std::string input, command;
+
+    while (true) {
+        std::getline(std::cin, input); 
+        std::stringstream ss(input);
+        ss >> command;
+
+        if (command == "uci") {
+            handle_uci();
+        }
+        else if (command == "isready") {
+            handle_isready();
+        }
+        else if (command == "ucinewgame") {
+            handle_ucinewgame();
+        }
+        else if (command == "position") {
+            handle_position(ss);
+        }
+        else if (command == "go") {
+            handle_go(ss);
+        }
+        else if (command == "quit") {
+            handle_quit();
+            break;
+        }
+    }
 }
 
 
